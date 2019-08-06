56bd2199e3017dbec0fc5d6840a58d0c90c3631f
MayerF <mingyangfan@58coin.com>
2019-08-06 11:44:19 +0800

    delete pods/

--- Example/Pods/Target Support Files/Pods-SegmentsControl_Example/Pods-SegmentsControl_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SegmentsControl_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SegmentsControl_Example
-@end
--- Example/Pods/Target Support Files/SegmentsControl/SegmentsControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SegmentsControl : NSObject
-@end
-@implementation PodsDummy_SegmentsControl
-@end
